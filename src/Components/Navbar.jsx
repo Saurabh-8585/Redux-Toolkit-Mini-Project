42d1c4baf33b912721380e1d38d2bbe05372b26a
Saurabh Khatmode <KHATMODESAURABH7@GMAIL.COM>
2023-05-18 19:34:43 +0530

    initialized reduc store ,created slice for cart and display those product in cart page and the count

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
 import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+    const cart = useSelector((state) => state.cart);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const handleMenuToggle = () => {
-      setIsMenuOpen(!isMenuOpen);
+        setIsMenuOpen(!isMenuOpen);
     };
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
@@ -45,10 +47,9 @@ const Navbar = () => {
                         />
                     </svg>
                 </button>
-                <div   className={`${
-            isMenuOpen ? 'block' : 'hidden'
-          } w-full md:block md:w-auto`}
-          id="navbar-default">
+                <div className={`${isMenuOpen ? 'block' : 'hidden'
+                    } w-full md:block md:w-auto`}
+                    id="navbar-default">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
                             <NavLink
@@ -72,10 +73,10 @@ const Navbar = () => {
                                 to="/cart"
                                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                             >
-                                Cart
+                                Cart({cart.length})
                             </NavLink>
                         </li>
-                     
+
                     </ul>
                 </div>
             </div>
